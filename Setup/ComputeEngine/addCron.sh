10a1d8562ff748bb6d1aed03bede5aa2b5c00fc9
githubmobiliya <31727151+githubmobiliya@users.noreply.github.com>
2018-06-15 17:07:20 +0530

    added cron- reboot and delete images

---
+++ Setup/ComputeEngine/addCron.sh
@@ -0,0 +1,14 @@
+sed -i -e 's/\r$//' cleanJob.sh
+sed -i -e 's/\r$//' rebootServerJetson.sh
+sudo chmod +x cleanJob.sh
+sudo chmod +x rebootServerJetson.sh
+mv ./cleanJob.sh ~/
+mv ./cleanupImages.py ~/
+mv ./rebootServerJetson.sh ~/ 
+crontab -l > mycron
+#echo new cron into cron file
+echo "* * * * * ./cleanJob.sh" >> mycron
+echo "@reboot ./rebootServerJetson.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
