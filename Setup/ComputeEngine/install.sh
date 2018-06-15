bfeb40171b91b41e1c5690cf0066d81f6d76c263
githubmobiliya <31727151+githubmobiliya@users.noreply.github.com>
2018-06-15 17:04:26 +0530

    updated installation file with repo name,comments

--- Setup/ComputeEngine/install.sh
+++ Setup/ComputeEngine/install.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -r=*|--repo=*)
-    REPO_URL="${i#*=}"
-    shift
-    ;;
-esac
-done
-echo "REPO  = ${REPO_URL}"
+BASE_DIR=${PWD}
 
-sudo apt-get install git cmake curl aptitude
+sudo apt-get install -y git cmake curl aptitude
+
+cd ~/
+if [[ -d "ComputeEngine" ]]; then
+    echo "In project Directory"
+ sudo rm -rf ComputeEngine
+ fi
+
+git clone https://snsguest:Mobgit12%21%40@github.com/MobiliyaTechnologies/ComputeEngine
+if [ "$?" -eq "0" ]
+then
+echo -e "\n*************ComputeEngine clone success"
+else
+	exit 1;
+fi
+
+echo -e "\n*************Adding cron"
+cd ${BASE_DIR}
+sed -i -e 's/\r$//' addCron.sh 
+sudo chmod +x addCron.sh
+./addCron.sh
+cd ~/
 
 #Install NodeJS
+echo -e "\n*************Installing NodeJs"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
@@ -28,41 +41,35 @@ sudo apt-get install -y libcurl4-gnutls-dev
 sudo npm install -y forever -g
 
 #Installation of opencv-3.2.0
-sudo apt-get purge -y libopencv4tegra-python libopencv4tegra-dev libopencv4tegra
-sudo apt-get purge -y libopencv4tegra-repo
+sudo aptitude purge -y libopencv4tegra-python libopencv4tegra-dev libopencv4tegra
+sudo aptitude purge -y libopencv4tegra-repo
 sudo apt-get purge -y python-numpy
 sudo apt autoremove -y
-sudo apt-get install gir1.2-gstreamer-1.0 gstreamer-tools gstreamer1.0-alsa gstreamer1.0-clutter gstreamer1.0-clutter-3.0 gstreamer1.0-fluendo-mp3 gstreamer1.0-nice gstreamer1.0-plugins-bad gstreamer1.0-plugins-bad-faad gstreamer1.0-plugins-bad-videoparsers gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-ugly-amr gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-x libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-base1.0-0 libgstreamer-plugins-good1.0-0 libgstreamer1.0-0 libgstreamer1.0-dev libreoffice-avmedia-backend-gstreamer  gstreamer1.0-libav
+sudo apt-get install -y gir1.2-gstreamer-1.0 gstreamer-tools gstreamer1.0-alsa gstreamer1.0-clutter gstreamer1.0-clutter-3.0 gstreamer1.0-fluendo-mp3 gstreamer1.0-nice gstreamer1.0-plugins-bad gstreamer1.0-plugins-bad-faad gstreamer1.0-plugins-bad-videoparsers gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-ugly-amr gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-x libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-base1.0-0 libgstreamer-plugins-good1.0-0 libgstreamer1.0-0 libgstreamer1.0-dev libreoffice-avmedia-backend-gstreamer  gstreamer1.0-libav
 
-cd ${PWD}/installation/base64-simple
-make
-sudo make install
-
-#Remove this add our sh of buildOpencv
 #OpenCV Installation
-cd ${PWD}/installation/installComputeEngineYOLO
+echo -e "\n*************Installing OpenCV"
+cd ${BASE_DIR}
+sed -i -e 's/\r$//' buildOpenCV.sh
 sudo chmod u+x buildOpenCV.sh
 ./buildOpenCV.sh
 cd ~/opencv/build
+sudo make install 
+
+echo -e "\n*************Installing base64 library"
+cd ${BASE_DIR}
+cd ./base64-simple
+make
 sudo make install
 
-cd ~/
-if [[ -d "ComputeEngine" ]]; then
-    echo "In project Directory"
- sudo rm -rf ComputeEngine
- fi
-git clone ${REPO_URL}/ComputeEngine
-
 #Installation of project 
+echo -e "\n*************Let's install Compute Engine"
 cd ~/ComputeEngine/darknet
+echo -e "\n*************Downloading Model weights"
 wget https://pjreddie.com/media/files/yolo.weights
-make
-
-cd ~/ComputeEngine/jetsonNodeServer
+make -j4
+cd  ~/ComputeEngine/jetsonNodeServer
+echo -e "\n*************Installing Node Server"
 sudo npm install
 
-cd ${PWD}/installation/installComputeEngineYOLO
-sudo chmod +x addCronComputeEngine.sh
-./addCronComputeEngine.sh
-
-echo "Installation is done..Do configuration step"
+echo "Compute Engine Installation is done..Do the configuration in settings.js file"
